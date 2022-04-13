567fa66a13c21b9d4746ebacd02d1a953e89d6d4
Jesse Christensen <jtq.christensen@gmail.com>
2022-04-13 10:32:33 -0700

    main page styling, added links to functions being implemented

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from 'react-redux';
+import { NavLink } from "react-router-dom";
 
 import './HomePage.css'
 
@@ -66,6 +67,50 @@ const HomePage = () => {
       <div className="side-page-cont">
 
         <div className="side-page">
+
+          <div className='side-header'>
+
+            <div id="banner">Top Communities</div>
+
+            <ol style={{"list-style": "none"}}>
+
+              {communities?.map((community) => {
+                return(
+                  <li className="top-com-list" key={community?.id}>
+                      <NavLink to={`/s/${community?.name}`}>{community?.name}</NavLink>
+                  </li>
+
+                )
+              })}
+
+            </ol>
+
+          </div>
+
+          <div className="main-spacer"></div>
+
+          <div className="side-header">
+
+            <div className="personal-home-cont">
+
+
+              <div>Home</div>
+
+              <div>Your personal Saidit frontpage. Come here to check in with your favorite communities.</div>
+
+              <div id="create-links">
+
+                <button style={{"backgroundColor": "#0079D3", "color": "#FFF", "borderColor": "#0079D3"}} className="main-links">Create Post</button>
+                <NavLink to='/communities/new'>
+                  <button style={{"backgroundColor": "#FFF", "color": "#0079D3", "borderColor": "#0079D3"}} className="main-links">Create Community</button>
+                </NavLink>
+              </div>
+
+            </div>
+
+
+
+          </div>
 
 
         </div>
